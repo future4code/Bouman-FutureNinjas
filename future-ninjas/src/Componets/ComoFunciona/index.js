8921552d9079efbb20a5e329a63bdc9aa412e0a6
rafacazal <rafaelacazal12@gmail.com>
2019-12-18 14:16:24 -0300

    página Como Funciona

--- future-ninjas/src/Componets/ComoFunciona/index.js
+++ future-ninjas/src/Componets/ComoFunciona/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import LogoFutureNinja from '../../Resources/logo.png'
 import LogoFacebook from '../../Resources/facebook.png'
 import LogoTwitter from '../../Resources/twitter.png'
-import LogoEmail from '../../Resources/email.png'
+import LogoEmail from '../../Resources/Email.png'
 import LogoInstagram from '../../Resources/instagram.png'
 import styled from 'styled-components';
 
@@ -17,7 +17,7 @@ margin: 2vw 4vw;
 `
 const Banner= styled.section`
 display: flex;
-height: 25rem;
+height: 24rem;
 width: 100%;
 background-color: #F5F3FB;
 `
@@ -64,7 +64,7 @@ position: absolute;
 height: 38px;
 width: 15vw;
 left: 9vw;
-bottom: 20vw;
+bottom: 21.8vw;
 font-style: normal;
 font-weight: 600;
 font-size: 17px;
@@ -85,12 +85,46 @@ bottom: 0;
 const Logos = styled.section`
 display: flex;
 position: absolute;
-left: 50%;
-bottom: 5vw;
+left: 40%;
+bottom: 3vw;
+margin: 1vw 1vw 1vw 1vw;
 `
 const Instagram = styled.img`
-width:75px;
-height: 75px;
+width:60px;
+height: 60px;
+`
+const Facebook = styled.img`
+width:60px;
+height: 60px;
+`
+const Email = styled.img`
+width: 59px;
+height: 59px;
+`
+const Twitter = styled.img`
+width:60px;
+height: 60px;
+`
+const BlocoTexto = styled.section`
+position: absolute;
+left: 9vw;
+width: 80vw;
+height: 11vw;
+bottom: 8vw;
+padding: 3px;
+margin-bottom: 20px;
+margin-top: 10px;
+background-image: linear-gradient(white, #F5F5F5);
+`
+const Texto = styled.p`
+color: #404040;
+font-weight: 550;
+font-size: 17px;
+font-style: regular;
+text-align: justify;
+padding: 5px;
+margin-left: 6px;
+margin-right: 6px;
 `
 export default class ComoFunciona extends Component {  
    render() {
@@ -105,9 +139,14 @@ export default class ComoFunciona extends Component {
             <TextoCard> Como Funciona? </TextoCard>
 
             <Logos>
-            <Instagram src={LogoInstagram}/>
+            <Instagram alt="Instagram" src={LogoInstagram}/>
+            <Facebook alt="Facebook"  src={LogoFacebook}/>
+            <Email alt="E-mail"  src={LogoEmail}/>
+            <Twitter alt="Twitter"  src={LogoTwitter}/>
             </Logos>
-
+            <BlocoTexto> 
+               <Texto>Sabemos o quanto é difícil achar um profissional de confiança para desenvolver serviços que não temos muito domínio. Assim nasce a FutureNinjas com a proposta de facilitar o encontro da tarefa e a resolução: você e um profissional ninja. Você não precisa se preocupar com nada: basta inscrever o seu serviço e um dos nossos ninjas entrará em contato em até 24hrs. O agendamento e pagamento será feito tudo dentro da nossa plataforma garantindo mais segurança para ambas as partes. E sempre ao final do serviço não se esqueça de avaliar seu ninja.</Texto>
+            </BlocoTexto>
             <Footer/>
 
          </PageContainer>
